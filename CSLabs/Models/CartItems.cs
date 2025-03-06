91cd1fa8da89e4b113a5697dece4269774710fb0
morrs <grishkovea@gmail.com>
2025-03-07 01:29:43 +0300

    feat: добавил сущность товара в корзине

---
+++ CSLabs/Models/CartItems.cs
@@ -0,0 +1,9 @@
+﻿namespace CSLabs.Models;
+
+public class CartItem
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+    public int Total => Price * Quantity;
+}
