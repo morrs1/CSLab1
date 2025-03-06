b75641eb74e63ecfe5987cfa551253563381389d
morrs <grishkovea@gmail.com>
2025-03-07 01:28:42 +0300

    feat: реализовал сущность товара

---
+++ CSLabs/Models/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CSLabs.Models;
+
+public class ProductModel(string productName, int price, string description)
+{
+    public string Name { get; set; } = productName;
+    public int Price { get; set; } = price;
+    public string Description { get; set; } = description;
+}
